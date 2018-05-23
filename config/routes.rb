347dab204412da67644bbfe8d49753e71a04048f
Anthony McShane <a89th@hotmail.com>
2018-05-23 15:33:21 +0100

    navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end
+
+  root to: 'restaurants#index'
+
+  # GET "restaurants", to: "restaurants#index"
+
+  # GET "restaurants/new", to: "restaurants#new"
+  # POST "restaurants", to: "restaurants#create"
+
+  # GET "restaurants/:id", to: "restaurants#show"
+
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name: "McDonalds",
+    address: "Rua das Madres",
+    phone_number: "012412",
+    category: "belgian"
+  },
+   {
+    name: "Wendys",
+    address: "Rua Sao Paulo",
+    phone_number: "0124124352",
+    category: "italian"
+  },
+     {
+    name: "KFC",
+    address: "Las Vegas boulevard",
+    phone_number: "0124121244352",
+    category: "chinese"
+  },
+    {
+    name: "Burger King",
+    address: "Central station",
+    phone_number: "333",
+    category: "french"
+  },
+  {
+    name: "The Fat Duck",
+    address: "England",
+    phone_number: "3323423",
+    category: "french"
+  }
+]
+
+restaurants_attributes.each { |params| Restaurant.create!(params)}
